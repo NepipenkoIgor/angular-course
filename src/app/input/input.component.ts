5574490cab4ba8801602d8b5aa7fd025c7526e08
Nepipenkoigor <igornepipenko@gmail.com>
2016-10-18 22:14:53 +0300

    lesson-3

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,4 +1,6 @@
 import {Component, Directive, Input, Output, EventEmitter, ElementRef} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {Observer} from 'rxjs/Observer';
 
 
 @Directive({
@@ -29,17 +31,34 @@ export class LogDirective {
 })
 export class InputComponent {
 
-  public isLogged: boolean = true;
-  public width: number = 200;
 
-  public accounts = [
-    {id: 3, name: 'Igor'},
-    {id: 2, name: 'Vova'},
-    {id: 1, name: 'Mike'},
-    {id: 4, name: 'Tanya'},
-    {id: 5, name: 'Ola'},
-  ];
-  public tab = 1;
+  public name = 'VOVA';
+
+  public obj = {
+    name:'Vlad',
+    id:3
+  }
+  public a:number = 0.2345;
+  public b:number = 1.2845;
+
+  public msg = new Promise<string>((resolve, reject) => {
+    setTimeout(()=>resolve('My msg'),3000)
+  })
+
+  public time = new Observable<string>((observer: Observer<string>) => {
+    setInterval(() => observer.next(new Date().toString()), 1000)
+  })
+  // public isLogged: boolean = true;
+  // public width: number = 200;
+  //
+  // public accounts = [
+  //   {id: 3, name: 'Igor'},
+  //   {id: 2, name: 'Vova'},
+  //   {id: 1, name: 'Mike'},
+  //   {id: 4, name: 'Tanya'},
+  //   {id: 5, name: 'Ola'},
+  // ];
+  // public tab = 1;
 
 
   @Input()
@@ -48,18 +67,15 @@ export class InputComponent {
   @Output()
   public myCustomEvent: EventEmitter<string> = new EventEmitter();
 
-  public emit(ev: MouseEvent) {
-    let el = ev.target as HTMLInputElement;
-    this.myCustomEvent.emit(el.value);
-  }
+  // public emit(ev: MouseEvent) {
+  //   let el = ev.target as HTMLInputElement;
+  //   this.myCustomEvent.next(el.value);
+  // }
 
-
-
-  /** обсудить!!!!!!  + ng-bind*/
-
-  public trackFn(index, item): any {
-    console.log(index, item)
-    return item.id
-  }
+  //
+  // public trackFn(index, item): any {
+  //   console.log(index, item)
+  //   return item.id
+  // }
 
 }
